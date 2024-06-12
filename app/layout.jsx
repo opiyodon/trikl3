627b2720b58ae983125ab52cd6170cd32f7e4aec
Don Artkins <opiyodon9@gmail.com>
2024-06-12 17:17:53 +0300

    modified the folder structure and the layout of the app and installed nextUI library and handled routing to different pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,22 @@
+import CommonLayout from "./CommonLayout";
+import Providers from "./Providers";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Trikl3</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
+      <body>
+        <Providers>
+            <CommonLayout>
+              {children}
+            </CommonLayout>
+        </Providers>
+      </body>
     </html>
   );
 }
---
+++ components/Trikl3Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Trikl3Logo = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Trikl3Logo
