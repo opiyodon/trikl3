a7f560c048a1261690e9251ffe93a2df8e4c7a0a
Don Artkins <opiyodon9@gmail.com>
2024-06-19 13:06:51 +0300

    styling the navbar

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -1,27 +1,37 @@
-import { Link, NavbarItem } from '@nextui-org/react'
-import React from 'react'
+import { Button, Link, NavbarItem } from '@nextui-org/react';
+import React from 'react';
 
 const NavItem = ({ activeItem, setActiveItem, route, name }) => {
-
-    const isActive = activeItem === route; // Check if the link is active
+    const isActive = activeItem === route;
 
     const handleClick = () => {
-        setActiveItem(route); // Call setActiveItem with the route when clicked
+        setActiveItem(route);
     };
 
     return (
-        <NavbarItem >
-            <Link
-                href={route}
-                aria-label={name}
-                onClick={handleClick}
-                className={`text-black font-normal hover:text-purple-800 transition-all duration-400 ease-in-out ${isActive ? "active" : ""
-                    }`}
-            >
-                {name}
-            </Link>
+        <NavbarItem>
+            {route === '/post-internships' ? (
+                <Button
+                    as={Link}
+                    href={route}
+                    aria-label={name}
+                    onClick={handleClick}
+                    className="bg-purple-800 rounded-full text-white font-bold hover:bg-purple-800 transition-all duration-400 ease-in-out"
+                >
+                    {name}
+                </Button>
+            ) : (
+                <Link
+                    href={route}
+                    aria-label={name}
+                    onClick={handleClick}
+                    className={`text-black font-bold hover:text-purple-800 transition-all duration-400 ease-in-out ${isActive ? 'active' : ''}`}
+                >
+                    {name}
+                </Link>
+            )}
         </NavbarItem>
-    )
+    );
 }
 
-export default NavItem
+export default NavItem;
--- test.jsx
+++ test.jsx
@@ -1,34 +1,48 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.jsx";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+import { SearchIcon } from "./SearchIcon.jsx";
 
 export default function App() {
   return (
-    <Navbar>
-      <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="secondary">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
+    <Navbar isBordered>
+      <NavbarContent justify="start">
+        <NavbarBrand className="mr-4">
+          <AcmeLogo />
+          <p className="hidden sm:block font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-3">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Features
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page" color="secondary">
+              Customers
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Integrations
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
       </NavbarContent>
 
-      <NavbarContent as="div" justify="end">
+      <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+        />
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
             <Avatar
